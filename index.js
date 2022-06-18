08ca25e8c9a1589b7a5b029ce4fb392ae54dd5b1
Frank-197 <frankomosh197@gmail.com>
2022-06-18 12:55:09 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+function reversedString(word){
+  const wordArray=word.split('');
+  const reversedWordArray=wordArray.reverse('');
+  const reversedWord=reversedWordArray.join('');
+  return reversedWord;
+}
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord=reversedString(word);
+  if (word===reversedWord){
+      return true
+  }else{
+    return false
+  }
+    
 }
 
 /* 
   Add your pseudocode here
+  reverse the input string 
+  if the reversed string is equal to the input
+    return true
+  else 
+     return false  
 */
 
 /*
   Add written explanation of your solution here
+  The isPalidrome function returns either true or false depending on whether the argument passed in the palidrome is a  function 
 */
 
 // You can run `node index.js` to view these console logs
